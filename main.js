d500ec75888b1b7c61f61ac486f65432c21eeb50
LOHAMER <130869756+LO-HAMER@users.noreply.github.com>
2024-01-10 23:53:27 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const sillySentence = (adjective, verb, noun ) =>  `I am so ${adjective} because I ${verb} coding! Time to write some more awesome ${noun}!`
+//adjective = 'excited';
+//verb = 'love';
+//noun = 'function';
+
+ console.log(sillySentence('excited', 'love', 'functions')) ;
+
